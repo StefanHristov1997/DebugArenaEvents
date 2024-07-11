c7d861c4bf2bc7e2020bcb6fba59679c5de2e82b
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-11 16:01:00 +0300

    Add initial commit.

---
+++ src/test/java/com/debugarenaevents/DebugArenaEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debugarenaevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugArenaEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
