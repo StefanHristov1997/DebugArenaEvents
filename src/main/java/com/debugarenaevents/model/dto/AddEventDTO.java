a48f00401a487b5b759362635d5e9fadbf56e9d5
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-07-12 14:59:02 +0300

    Add event validation.

--- src/main/java/com/debugarenaevents/model/dto/AddEventDTO.java
+++ src/main/java/com/debugarenaevents/model/dto/AddEventDTO.java
@@ -1,6 +1,7 @@
 package com.debugarenaevents.model.dto;
 
 import com.debugarenaevents.model.enums.VideoPlatformEnum;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,15 +11,26 @@ import java.time.LocalDateTime;
 @Setter
 public class AddEventDTO {
 
+    @NotBlank(message = "{blank_field_message}")
+    @Size(min = 6, message = "{title.length.message}")
     private String title;
 
+    @NotBlank(message = "{blank_field_message}")
+    @Size(min = 20, message = "{description.length.message}")
     private String description;
 
+    @NotBlank(message = "{blank_field_message}")
+    @Size(min = 6, message = "{authorName.length.message}")
     private String authorName;
 
+    @NotBlank(message = "{blank_field_message}")
+    @Email
     private String authorEmail;
 
+    @NotNull(message = "{date.not.null.message}")
+    @Future(message = "{valid.date.message}")
     private LocalDateTime date;
 
+    @NotNull(message = "{platform.not.null.message}")
     private VideoPlatformEnum platform;
 }
